2e08740a22aaa5e8d6b5db18b27e6b3c1594855e
NotWhoops <whoopsplayz@gmail.com>
2025-11-17 16:57:44 -0800

    final changes

---
+++ app.js
@@ -0,0 +1,65 @@
+const changecolorpurple = document.getElementById("change-color-purple");
+const changebackgroundimage = document.getElementById("change-background-image");
+const changeredptext = document.getElementById("change-red-ptext");
+const textrandom = document.getElementById("text-random");
+const changedivbackground = document.getElementById("change-div-background");
+const changetextimage = document.getElementById("change-text-image");
+const changeimagetext = document.getElementById("change-image-text");
+const changepbackground = document.getElementById("change-p-background");
+const activatealert = document.getElementById("activate-alert");
+
+    function changeColorPurple() {
+        document.body.style.background = 'purple'; 
+    }
+    function changeBackgroundImage() {
+        document.body.style.backgroundImage = "url(./assets/dog.png)";
+    }
+    function changeRedPText() {
+        document.body.style.color = 'red';
+    }
+    function changeDivBackground() {
+        document.body.style.background = 'green';
+    }
+    function changeTextImage() {
+        document.body.style.text = "I put creative words here";
+    }
+    function changeImageText() {
+        document.body.style.image = "url(./assets/dog.png)";
+    }
+    function changePBackground() {
+        document.body.style.background = "url(./assets/dog.png)";
+    }
+    function ActivateAlert() {
+        document.body.style.activatealert = "play-alert";
+    }
+    
+    
+    
+    const playAudio = document.getElementById('play-audio');
+    document.getElementById('playButton').addEventListener('click', function() {
+    if (playAudio.paused) {
+        playAudio.play();
+        this.textContent = 'Pause';
+    } else {
+        playAudio.pause();
+        this.textContent = 'Play';
+    }
+    });
+    
+    textrandom.addEventListener("click", () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.textDecorationColor = randomColor;
+    });
+
+
+
+    changecolorpurple.addEventListener('click', changeColorPurple);
+    changebackgroundimage.addEventListener('click', changeBackgroundImage);
+    changeredptext.addEventListener('click', changeRedPText);
+    textrandom.addEventListener('click', textrandom);
+    changedivbackground.addEventListener('click', changeDivBackground);
+    changetextimage.addEventListener('click', changeTextImage);
+    changeimagetext.addEventListener('click', changeImageText);
+    changepbackground.addEventListener('click', changePBackground);
+    activatealert.addEventListener('click', ActivateAlert);
+    playAudio.addEventListener('click',PlayAudio);
