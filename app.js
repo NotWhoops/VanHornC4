1750c4627e440ef6bb035b9432181614efbdcc65
NotWhoops <whoopsplayz@gmail.com>
2025-11-19 10:06:39 -0800

    fixed more buttons

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const changebackgroundimage = document.getElementById("change-background-image")
 const changeredptext = document.getElementById("change-red-ptext");
 const textrandom = document.getElementById("text-random");
 const changedivbackground = document.getElementById("change-div-background");
-const changetextimage = document.getElementById("change-text-image");
-const changeimagetext = document.getElementById("change-image-text");
+const makeacard = document.getElementById("make-a-card");
+const cardsContainer = document.getElementById("cards-container");
+const changewebsitetitle = document.getElementById("change-website-title");
 const changepbackground = document.getElementById("change-p-background");
 const activatealert = document.getElementById("activate-alert");
+const showimage = document.getElementById("show-image")
 
-function changeColorPurple() {
-    document.body.style.background = 'purple'; 
-}
+    function changeColorPurple() {
+        document.body.style.background = 'purple'; 
+    }
     function changeBackgroundImage() {
         document.body.style.backgroundImage = "url(./assets/dog.png)";
     }
@@ -20,24 +22,44 @@ function changeColorPurple() {
     function changeDivBackground() {
         document.getElementById("div").style.backgroundColor = 'green';
     }
-    function changeTextImage() {
-        document.body.style.text = "I put creative words here";
+    function makeACard(text) {
+        const card = document.createElement("div");
+        card.className = "card";
+        const content = document.createElement("p");
+        content.innerText = text;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete Card'";
+        deleteBtn.addEventListener("click",() => {
+        card.remove();
+    });
+        card.appendChild(content);
+        card.appendChild(deleteBtn);
+        return card;
     }
-    function changeImageText() {
-        document.body.style.image = "url(./assets/dog.png)";
+    function changeWebsiteTitle() {
+        document.title = "You changed the title!";
     }
     function changePBackground() {
-        document.getElementById("change-p-background").style.image = "url(./assets/dog.png)";
+        document.getElementById("ptag").style.backgroundImage = "url(./assets/dog.png)";
     }
     function ActivateAlert() {
         document.getElementById("activate-alert").style.alert = alert("YaY! Alert Time!");
     }
-    
+    function ShowImage() {
+        document.getElementById("cat-image").style.backgroundImage = "url(./assets/cat.jpg)";
+    }
+
+
+    makeacard.addEventListener("click", () => {
+    const card = makeACard("You have a card now!");
+    cardsContainer.appendChild(card);
+    });
+
+
     textrandom.addEventListener("click", () => {
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.color = randomColor;
     });
-
 
 
     changecolorpurple.addEventListener('click', changeColorPurple);
@@ -45,7 +67,8 @@ function changeColorPurple() {
     changeredptext.addEventListener('click', changeRedPText);
     textrandom.addEventListener('click', textrandom);
     changedivbackground.addEventListener('click', changeDivBackground);
-    changetextimage.addEventListener('click', changeTextImage);
-    changeimagetext.addEventListener('click', changeImageText);
+    makeacard.addEventListener('click', makeACard);
+    changewebsitetitle.addEventListener('click', changeWebsiteTitle);
     changepbackground.addEventListener('click', changePBackground);
     activatealert.addEventListener('click', ActivateAlert);
+    showimage.addEventListener('click', ShowImage);
